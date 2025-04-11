9dafc9f1dcc346dcc85561707782e92d73fa4e15
alej00p <alejandrodel207@gmail.com>
2025-04-11 17:54:32 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-patient-write-alejandro-774.onrender.com"],  # Permitir solo este dominio
+    allow_origins=["https://hl7-patient-write-Alejandro-774.onrender.com"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
